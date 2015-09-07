15e1c782d35f00c6f6f7d634b4d82b06cc9d8458
Allen Hernandez <allensh12@users.noreply.github.com>
2015-09-07 16:24:13 -0700

    moves gongs into config.

--- src/index.js
+++ src/index.js
@@ -5,27 +5,56 @@
 var fs = require('fs');
 var wav = require('wav');
 var Speaker = require('speaker');
+var gongs = require('./gongs.json');
+var GRID_SIZE = 4;
 
 var midiConnector = require('midi-launchpad').connect(0);
 
 midiConnector.on('ready', function(launchpad) {
   console.log('It\'s gong time');
 
+  showGongChoices(launchpad);
+});
+
+function showGongChoices(launchpad) {
+  var sounds = {};
+
+  gongs.map(function(elem, i) {
+    var row = Math.floor(i / GRID_SIZE);
+    var col = i % GRID_SIZE;
+    var button;
+
+    if (row >= GRID_SIZE) {
+      console.log('Warning: Too many gongs. Ignoring gong - ' + elem);
+    } else {
+      sounds[row] = sounds[row] || {};
+      sounds[row][col] = elem;
+
+      button = launchpad.getButton(row, col);
+      button.light(launchpad.colors.green.high);
+    }
+  });
+
   launchpad.on('press', handlePress);
   launchpad.on('release', handleRelease);
 
   function handlePress(button) {
-    button.light(launchpad.colors.green.high);
+    button.dark();
   }
 
   function handleRelease(button) {
-    button.dark();
-    play();
+    if (sounds[button.x] && sounds[button.x][button.y]) {
+      play(sounds[button.x][button.y]);
+      button.light(launchpad.colors.green.high);
+    } else {
+      console.log('Warning: No gong for cell (' + button.x + ', ' + button.y + ')');
+    }
   }
-});
+}
 
-function play() {
-  var file = fs.createReadStream('./src/assets/gong.wav');
+// TODO: Allow to choose b/w multiple gong sounds
+function play(sound) {
+  var file = fs.createReadStream(sound);
   var reader = new wav.Reader();
 
   reader.on('format', function(format) {
@@ -35,4 +64,3 @@ function play() {
 
   file.pipe(reader);
 }
-
